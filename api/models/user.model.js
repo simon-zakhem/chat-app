7a67c74c138d4a0d0028d50179da2f6fe4371e39
simonzakhem <simon.zakhem@std.balamand.edu.lb>
2024-07-31 15:15:28 +0300

    Fixed user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true },
     password:{ type: String, required: true, minlength: 6 },
     profilePic: { type: String, default: "" },
-})
+}, {timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 
